460df80a5f44ddda10faebe979cba8763aee8070
Ubuntu <ubuntu@ip-172-31-32-165.eu-north-1.compute.internal>
2026-03-07 19:56:19 +0000

    Added Java Docker project

---
+++ simple-java-docker/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Docker ! with the java image we have created the conatainer with the help of dockerfile. Dockerfile is nothing but a compile a proccess code in whch written the each step and clear perform the java app");
+    }
+}
